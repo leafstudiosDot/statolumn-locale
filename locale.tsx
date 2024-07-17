a59daa6fe7c11eb58baccfb251e9119523883bc7
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2024-07-17 09:22:57 +0800

    Added Japanese and Modified English

--- locale.tsx
+++ locale.tsx
@@ -2,6 +2,7 @@
  
 const locales = {
   en: () => import('./locale/en.json').then((module) => module.default),
+  jp: () => import('./locale/jp.json').then((module) => module.default)
 }
  
 export const getLocale = async (locale) => locales[locale]()
