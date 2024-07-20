1071dab1c12df4215eb480f72bea0b19db4b787e
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2024-07-20 20:51:19 +0800

    Added Display Name

--- locale.tsx
+++ locale.tsx
@@ -6,3 +6,20 @@ const locales = {
 }
  
 export const getLocale = async (locale) => locales[locale]()
+
+export const getLanguageDetails = async (currentLocale) => {
+  const localeData = await getLocale(currentLocale)
+  const otherLangNames = localeData.Language.OtherLangName.map(otherLang => ({
+    code: otherLang.LanguageCode,
+    name: otherLang.Name,
+    displayName: otherLang.DisplayLangName
+  }))
+  return [
+    {
+      name: localeData.Language.Name,
+      code: localeData.Language.Code,
+      displayName: localeData.Language.Name
+    },
+    ...otherLangNames
+  ]
+}
